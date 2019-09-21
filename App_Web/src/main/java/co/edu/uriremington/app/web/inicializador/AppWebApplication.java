fe6a34d3df097d6f9ae67e90fb448c09cd85c949
DANILOMONTOYA <danilo9831montoya@gamil.com>
2019-09-21 13:29:14 -0500

    Arqueotpo inicial del proecto

---
+++ App_Web/src/main/java/co/edu/uriremington/app/web/inicializador/AppWebApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uriremington.app.web.inicializador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebApplication.class, args);
+	}
+
+}
---
+++ LengProgr_Datos/src/co/edu/uniremington/lengprog/app/datos/package-info.java
@@ -0,0 +1,1 @@
+package co.edu.uniremington.lengprog.app.datos;
